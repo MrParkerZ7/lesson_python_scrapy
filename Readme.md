bc5167e87a2d599b3f723f7338540570e704628f
MrParkerZ7 <neverzero9@gmail.com>
2022-04-09 12:53:24 +0700

    [updating] selenium lazada

--- Readme.md
+++ Readme.md
@@ -2,9 +2,17 @@
 
 ### Tutoiral : https://www.youtube.com/watch?v=ogPMCpcgb-E
 
+### prepare env
+
 ```bash
-$ cd training_python_scrapy
+$ pip3 install -r requirements.txt
+```
+
+### run scrapy
+
+```bash
 $ scrapy crawl reddit
 $ scrapy crawl github
 $ scrapy crawl youtube
+$ scrapy crawl youtube_se
 ```
--- training_python_scrapy/spiders/selenium_lazada.py
+++ training_python_scrapy/spiders/selenium_lazada.py
@@ -9,35 +9,29 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 class RedditScrapy(scrapy.Spider):
-    name: str = 'se_youtube'
-    # start_urls: str = ['https://www.youtube.com/']
-    # fileNo: int = 0
+    name: str = 'youtube_se'
+    fileNo: int = 0
 
     def start_requests(self):
-        settings = get_project_settings()
-        # driver_path = settings['CHROME_DRIVER_PATH']
-        # driver_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-        # driver_path = 'D:\env_storeage\chrome_driver\chromedriver'
-        driver_path = binary_path
         options = webdriver.ChromeOptions()
         options.headless = True
-        # driver = webdriver.Chrome(driver_path, options=options)
         driver = webdriver.Chrome(
             ChromeDriverManager().install(), options=options)
-        # driver = webdriver.Chrome( options=options)
 
-        driver.get('https://www.youtube.com/')
+        driver.get('https://www.lazada.com.ph/shop-laptops/')
         link_elements = driver.find_elements_by_xpath(
             '//*[@data-qa-locator="product-item"]//a[text()]')
+        # link_elements = driver.find_elements_by_xpath(
+        # '//a/@href')
 
         for link in link_elements:
             yield scrapy.Request(link.get_attribute('href'), callback=self.parse)
-
         driver.quit()
 
     def parse(self, response: HtmlResponse, **kwargs):
-        print('XXX', response)
-        save_links_to_file(self, response.xpath("//a/@href"))
-        save_links_to_file(self, response.xpath("//link/@href"))
-        save_links_to_file(self, response.xpath("//img/@src"))
+        print('xxx', type(response))
+        # print('sss', response.xpath("//button/@class"))
+        # save_links_to_file(self, response.xpath("//button/@class"))
+        # save_links_to_file(self, response.xpath("//link/@href"))
+        # save_links_to_file(self, response.xpath("//img/@src"))
         pass
